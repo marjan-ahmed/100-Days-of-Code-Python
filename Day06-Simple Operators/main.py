c7afb503445f88ee5a7aa42cadcd3aa0dc6ba98a
Marjan-ahmed <marjan.ahmed08@yahoo.com>
2024-04-25 20:38:14 +0500

    Initial Commit

---
+++ Day06-Simple Operators/main.py
@@ -0,0 +1,11 @@
+# varibles
+a = 2
+b = 3
+
+print("Addition:",a + b)   # 2+3
+print("Substracton:",a - b)   # 2-3
+print("Multiplication:",a * b)   # 2*3
+print("Devision:",a / b)   # 2/3
+print("Floor Devision:",a // b)   # 0 (round the nearest value)
+print("Exponential:",a ** b)   # 2*2*2
+print("Modulus:",a % b)   # 2/3 = 2 (reminder)
