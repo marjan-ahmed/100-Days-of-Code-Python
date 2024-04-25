0fa1fd89eb18595d07836892e5f0f7804da6f8e4
Marjan-ahmed <marjan.ahmed08@yahoo.com>
2024-04-25 21:40:30 +0500

    Initial Commit

---
+++ Day08-Typecasting in Python/main.py
@@ -0,0 +1,22 @@
+# Explicit typecasting:
+
+# Eg: 1
+a = 4
+b = 5
+
+typeCasting1 = str(a) + str(b)
+print(typeCasting1)
+print(int(a) + int(b))
+
+# Eg: 2
+c = "Apple"
+
+print(list(c))
+print("\n")
+
+# Implicit TypeCasting:
+
+c = 1.9
+d = 8
+
+print(c + d)
---
+++ Day09-Taking User Input/main.py
@@ -0,0 +1,24 @@
+# basic code
+
+name = input("Enter your name: ")
+print("Welcome",name)
+print("\n")
+
+# logical codes
+
+print(" \t Problem")
+num1 = input("Enter your first number: ")
+num2 = input("Enter your second number: ")
+print(num1 + num2)
+print("\n")
+
+# If you input a str into input of int then It will be wrong
+# What is this? Don't Worry about it
+
+# Solution
+print(" \t Solution")
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your second number: "))
+print(num1 + num2)
+
+# Python has str() and float() also
