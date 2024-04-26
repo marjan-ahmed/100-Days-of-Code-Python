da4d6ed3e2d98c1b2c007c05c15321c40dc4c19f
Marjan-ahmed <marjan.ahmed08@yahoo.com>
2024-04-26 18:00:46 +0500

    Initial Commit

---
+++ Day11-Operation on String - Slicing String/main.py
@@ -0,0 +1,22 @@
+name = "Marjan"
+
+print(name[2])
+print(name[0:4]) # form (starting) : to (ending)
+
+print(len(name)) # the characters length of the word: len([word])
+print(len("Marjanahmed"))
+
+print(name[:3]) # It will compile 0 automatically 
+print(name[0:]) # The whole letters will be considered as the length of the word
+print(name[:]) # from starting It will put 0 till the length of the word
+print(name[0:-4]) # So bacisally It works like this: print(name[0:len(name)-4])
+print(name[0:len(name)-4])
+print(name[1:-3]) # The output will be ar
+
+
+#--- #--- #--- Quick Quiz ---# ---# ---#
+
+# a = "YourName"
+# print(len(a))
+# print(a[-6:-3]) 
+
